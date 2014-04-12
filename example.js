679018331bb567c3c8982168e1888df289adec21
James Spencer <jamess237@icloud.com>
2014-04-12 19:52:09 +1000

    lint example, and add to external file

--- example.js
+++ example.js
@@ -1,15 +1,3 @@
-node-hlld
-=========
-
-A node.js client for https://github.com/armon/hlld
-
-## Installation
-
-`npm install hlld`
-
-## Usage
-
-```javascript
 var hlld = require('hlld');
 var client = new hlld();
 var assert = require('assert');
@@ -28,23 +16,3 @@ client.create(['hyperloglog'], function (err, okay) {
     });
   });
 });
-```
-
-## API
-
-### `hlld()`
-    Returns a new hlld instance.
-#### `hlld().x`
-    Calls hlld method `x`
-
-## TODO
-- Connection Pool
-- Benchmark
-- Client-side hash
-- Bulk operations when possible
-- Maintain a set of open connections to the server
-- Better API documentation and test coverage
-- Add more TODOs :wink:
-
-## Licence
-MIT
